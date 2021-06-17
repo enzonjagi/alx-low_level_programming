3466c0d99fca0c1e38d8c413fa128707c28673f1
enzonjagi <amosndungo@gmail.com>
2021-06-17 07:30:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * main function - prints out a string of text
+ * main - prints out a string of text
+ * @void: the placeholder for empty parameters
+ * 
+ * Return: 0
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
