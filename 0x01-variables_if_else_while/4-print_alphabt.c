22c8bd1547745ab9edd325b6522aa4e3849f2e7b
enzonjagi <amonsdungo@gmail.com>
2021-06-18 17:17:45 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabet in lower case
+ * @void: placeholder for returning null
+ *
+ * description:
+ *    main(void); --> 0
+ * The main function prints out the alphabet in lowercase
+ * with the exception of 'q' and 'e'
+ * Does this using the putchar function
+ * that receives a char as it's input
+ * After which it prints out a newline
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'e')
+	{
+	ch++;
+	}
+	else if (ch == 'q')
+	{
+	ch++;
+	}
+	else
+	{
+	putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
