2abcc8ed197b1709202dd0ed170223d08bf309b3
enzonjagi <amonsdungo@gmail.com>
2021-06-18 14:51:18 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,9 +7,9 @@
  * main - assigns a random value to n each time it is executed
  * @void: the placeholder for empty parameters
  *
- * description: The main function assigns a random value on each execution
- * then checks to see if n is greater than 0: prints is positive
- * else if n is equal to 0: prints is zero
+ * Description: The main function assigns a random value on each execution
+ * then checks to see if n is greater than 0 prints it is positive
+ * else if n is equal to 0 it prints is zero
  * else it prints is negative
  * Return: 0
  */
