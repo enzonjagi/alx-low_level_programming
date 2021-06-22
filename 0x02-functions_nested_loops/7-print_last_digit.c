a0d7d7f3890bd9e113bccb17f7535c569f1201ad
enzonjagi <amonsdungo@gmail.com>
2021-06-23 01:22:06 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -19,9 +19,9 @@ int print_last_digit(int c)
 {
 int last_digit = c % 10;
 
-if (n < 0)
+if (c < 0)
 {
-last_digit = last * -1
+  last_digit = last * -1;
 }
 _putchar(last_digit + '0');
 return (last_digit);
