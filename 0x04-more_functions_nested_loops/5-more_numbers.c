bad34617f84adc4adddd2e7ba3adbb0e07c90ee0
enzonjagi <amonsdungo@gmail.com>
2021-06-24 21:43:08 +0300

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,11 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
-if(j < 10)
-{
-_putchar((j % 10) + '0');
-}
-else
+if (j > 10)
 {
 _putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
 }
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
