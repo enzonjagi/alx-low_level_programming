cb472f8732955682fa3dc3e82e437616ec109542
enzonjagi <amonsdungo@gmail.com>
2021-06-29 00:03:59 +0300

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,14 +16,14 @@ void puts_half(char *str)
 	if (length % 2 != 0)
 	{
 		/*print n = (length - 1) / 2*/
-		for (half = (length - 1) / 2 ; half < length; half++)
+		for (half = length / 2 ; half < length; half++)
 		{
 			_putchar(*(str + half));
 		}
 	}
 	else
 	{
-		for (half = (length - 1) / 2 ; half < length; half++)
+		for (half = length / 2 ; half < length; half++)
 		{
 			_putchar(*(str + half));
 		}
