ed99022a93b69eda4081e49a4ffb756e85b13beb
enzonjagi <amonsdungo@gmail.com>
2021-06-28 23:06:35 +0300

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: string passed as a parameter
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char reversed;
+
+	len = strlen(s);
+
+	for (i = (len - 1); i >= 0; i--)
+	{
+		reversed = *(s + i);
+	}
+	/*_putchar('\n');*/
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -6,5 +6,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
+void rev_string(char *s);
+void print_array(int *a, int n);
 
 #endif /* HOLBERTON_H */
