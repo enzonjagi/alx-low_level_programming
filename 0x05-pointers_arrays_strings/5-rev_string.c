a22802ef66e3992faadf0c847357b8ed2bc4e507
enzonjagi <amonsdungo@gmail.com>
2021-06-29 08:28:26 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,12 @@
 void rev_string(char *s)
 {
 	int len, i;
-	char reversed;
 
 	len = strlen(s);
 
 	for (i = (len - 1); i >= 0; i--)
 	{
-		reversed = *(s + i);
+		_putchar(*(s + i));
 	}
-	/*_putchar('\n');*/
+	_putchar('\n');
 }
