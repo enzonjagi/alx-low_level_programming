d7e5a5457a0b0a8311e11d3ffc5c587708188889
enzonjagi <amonsdungo@gmail.com>
2021-07-01 22:25:47 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,12 +10,12 @@
 char *leet(char *str)
 {
 	int i, j;
-	char check[] = "e E a A o O t T l L";
-	char repl[] = "3 3 4 4 0 0 7 7 1 1";
+	char check[] = "a A e E o O t T l L";
+	char repl[] = "4 4 3 3 0 0 7 7 1 1";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; check[i] != '\0' && repl[j] != '\0'; j++)
+		for (j = 0; check[j] != '\0' && repl[j] != '\0'; j++)
 		{
 			if (str[i] == check[j])
 			{
