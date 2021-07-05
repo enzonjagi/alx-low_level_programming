4cea34a1fdd509246895238824e089146b1d1363
enzonjagi <amosndungo@gmail.com>
2021-07-05 18:45:19 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strstr - checks if 'haystack' has the substring 'needle'
+ *           and returns the first occurence of this substring
+ * @haystack: string we're checking
+ * @needle: substring we're looking for
+ *
+ * Return: pointer to this occurence in @haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack != '\0')
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (*needle == *haystack)
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+	return ('\0');
+}
