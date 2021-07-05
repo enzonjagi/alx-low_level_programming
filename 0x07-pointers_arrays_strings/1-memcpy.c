1791cfdde15a3b90e5852a293cd0a8449988621a
enzonjagi <amosndungo@gmail.com>
2021-07-05 17:13:29 +0000

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,6 +12,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	/*unsigned int i = 0, j = 0;*/
+	char *ret = dest;
 
 	while (n--)
 	{
@@ -19,5 +20,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest = dest + 1;
 		src = src + 1;
 	}
-	return (dest);
+	return (ret);
 }
