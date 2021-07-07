0d133674c8f8760a408fa3a04cd59a1cc9d83a2e
enzonjagi <amosndungo@gmail.com>
2021-07-07 20:19:40 +0000

    minus loop for the checker

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,24 +1,4 @@
 #include "holberton.h"
-#include <string.h>
-
-/**
- * _strlen - checks the length of a string
- * @s: the string it receives
- *
- * Return: void
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
 
 /**
  * _print_rev_recursion - prints a string in reverse
