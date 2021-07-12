95149f1e30f88c7f800af1a798352bd25ff89a14
enzonjagi <amonsdungo@gmail.com>
2021-07-12 13:57:13 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed followed by a new line
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,6 @@
+#define UNUSED(x) (void)(x)
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#endif /* HOLBERTON_H */
