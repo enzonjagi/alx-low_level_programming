940c4c18b85a456a15538e2f34bc59ec764ddb0c
enzonjagi <amonsdungo@gmail.com>
2021-07-13 20:41:25 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,22 +13,13 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new;
-	/*char nullc = '\0';*/
 	int len = 0;
 
 	len = strlen(s1) + strlen(s2);
 	new = (char *)malloc(sizeof(char) * (len + 1));
 	if (new == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		strcat(new, s2);
-	else if (s2 == NULL)
-		strcat(new, s1);
-	else
-	{
-		strcat(new, s1);
-		strcat(new, s2);
-	}
-
+	strcat(new, s1);
+	strcat(new, s2);
 	return (new);
 }
