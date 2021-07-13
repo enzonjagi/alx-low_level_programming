c5af5c4a8ec8408843e5877ad5275f1ac09f10cb
enzonjagi <amonsdungo@gmail.com>
2021-07-13 17:01:35 +0300

    all

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,11 +11,11 @@ void print_number(int n)
 {
 if (n < 10 && n > -10)
 {
-_putchar((n % 10) + '0');
+_putchar((n % 10));
 }
 else
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+_putchar((n / 10));
+_putchar((n % 10));
 }
 }
