5d005eea1977e73e6146c1b74b8388716f82ef22
enzonjagi <amonsdungo@gmail.com>
2021-07-13 21:27:03 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,18 +13,17 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *ptr, *check;
+	char *ptr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc((sizeof(c)) * size);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
-	check = ptr;
-	if (check == NULL)
-		return (NULL);
 	return (ptr);
 }
