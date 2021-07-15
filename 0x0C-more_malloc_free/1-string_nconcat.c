c3847613474c671c8364841ab69986b62ac84332
enzonjagi <amonsdungo@gmail.com>
2021-07-15 21:11:52 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (new == NULL)
 		return (NULL);
 	i = 0;
-	while (str[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		new[i] = s1[i];
 		i++;
