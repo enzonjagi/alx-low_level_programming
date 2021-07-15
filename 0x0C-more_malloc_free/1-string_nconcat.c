9957b6a21a55be618be2cbdf3750a7d190ff0028
enzonjagi <amonsdungo@gmail.com>
2021-07-15 22:05:10 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,8 +25,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	if (n >= len2)
-		n = len2;
 
 	new = malloc(len1 + n + 1);
 	if (new == NULL)
