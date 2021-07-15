f0a14f2d4935d8cd971e9ca25eb1b2073d58426a
enzonjagi <amonsdungo@gmail.com>
2021-07-15 20:19:35 +0300

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *ptr;
 	unsigned int i;
 
-	if (size == 0 || nmemb == 0)
+	if (size == 0)
+		return (NULL);
+	if (nmemb == 0)
 		return (NULL);
 	ptr = malloc(nmemb * sizeof(size));
 
