fd6bcd4abccaf0539585e0052e8e9542201317cd
enzonjagi <amonsdungo@gmail.com>
2021-07-16 19:13:28 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,30 +16,19 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new;
-	unsigned int i, j;
-	unsigned int len1, len2, len;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	/*for (len1 = 0; s1[len1] != '\0'; len1++)
-	 *;
-	*for (len2 = 0; s2[len2] != '\0'; len2++)
-	*	;
-	*/
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-
 	if (n >= len2)
 		n = len2;
-
-	len = len1 + n + 1;
-	new = malloc(sizeof(*new) * len);
+	new = malloc(sizeof(*new) * (len1 + n + 1));
 	if (new == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 		new[i] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
@@ -47,8 +36,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		new[i] = s2[j];
 		i++;
 	}
-
 	new[i] = '\0';
-
 	return (new);
 }
