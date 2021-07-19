5b2d224a86e0fe2ef57b2cce1d7a8d3a7cb20409
enzonjagi <amonsdungo@gmail.com>
2021-07-19 22:41:53 +0300

    struct start

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: the dog's name
+ * @age: his or her age
+ * @owner: who's friend is this
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
