8844b39b1854b4067641e0cc4e44c812252d7fc4
enzonjagi <amonsdungo@gmail.com>
2021-07-20 00:19:25 +0300

    typedef update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: his or her age
  * @owner: who's friend is this
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
