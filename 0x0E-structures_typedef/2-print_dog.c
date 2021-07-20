751a42649d84a834107eb9972114b2ca020aa5ef
enzonjagi <amonsdungo@gmail.com>
2021-07-20 09:20:24 +0300

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,22 +13,24 @@ void print_dog(struct dog *d)
 		exit(EXIT_SUCCESS);
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
 	}
-	if (d->age <= 0)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: (nil)");
-		printf("Owner: %s\n", d->owner);
-	}
-	if (d->owner == NULL)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: (nil)");
-	}
+	/*else if (d->age <= 0)
+	 *{
+	 *	printf("Age: (nil)\n");
+	 *}
+	 *else if (d->owner == NULL)
+	 *{
+	 *	printf("Owner: (nil)\n");
+	 *}
+	 */
 	else
 	{
 		printf("Name: %s\n", d->name);
