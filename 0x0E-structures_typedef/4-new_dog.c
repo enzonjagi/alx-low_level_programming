410e26ad2f68ba83daf4822f9b780ca738fc3a17
enzonjagi <amonsdungo@gmail.com>
2021-07-21 23:45:30 +0300

    4 corrected I think

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
 		free(d);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
