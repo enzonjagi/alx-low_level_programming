74b8c6adb3f0bd03a52819a251d41c041de35d25
enzonjagi <amonsdungo@gmail.com>
2021-07-23 14:05:42 +0300

    One more addition

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct op - structure for types
+ * @ch: the type
+ * @f: function to be called
+ */
+typedef struct op
+{
+	char *ch;
+	void (*f)(va_list arg);
+} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
