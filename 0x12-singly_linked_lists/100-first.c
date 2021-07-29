fd365e4edd9f56f91cbf6fb5d7e5121fe6e9b33f
enzonjagi <amonsdungo@gmail.com>
2021-07-30 01:37:51 +0300

    before main huh

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before - prints a text before main is executed
+ * Return: void
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
