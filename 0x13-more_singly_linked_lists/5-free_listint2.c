ebe6013cac6bcd120f2e7b592bbf1e5dd296f84d
enzonjagi <amonsdungo@gmail.com>
2021-08-03 16:00:07 +0300

    Let's free this list again, shall we? Why bug me?

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
-
 	while (*head != NULL)
 	{
 		ptr = *head;
 		*head = ptr->next;
 		free(ptr);
 	}
-	/**head = NULL;*/
+	*head = NULL;
 	head = NULL;
 }
