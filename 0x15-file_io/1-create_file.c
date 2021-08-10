146801ec8c10243105b617ec9edeaba41ca03643
enzonjagi <amonsdungo@gmail.com>
2021-08-10 11:40:35 +0300

    1-create_file.c: debug one check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,22 +10,23 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i, count = 0;
+	ssize_t wr;
 
 	if (filename == NULL)
 		return (-1);
 	/*add a check; if file exists, truncate it*/
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		/*write(fd, "", 1);*/
 		close(fd);
 		return (1);
 	}
-	for (i = 0; text_content[i] != '\0'; i++)
-		count++;
-	write(fd, text_content, count);
+
+	wr = write(fd, text_content, count);
+	if (wr == -1)
+		return (-1);
 
 	close(fd);
 
